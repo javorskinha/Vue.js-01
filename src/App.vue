ff900287957abdcfe09eb04c9f8a31a786c43637
Javorskinha <andrielijavorski@gmail.com>
2025-02-18 16:04:43 -0300

    criando projeto vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import PrimeiroComponente from './components/PrimeiroComponente.vue';
+export default{
+  name: 'App',
+  components: {PrimeiroComponente},
+  data (){
+    return{
+      testando: "teste"
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <PrimeiroComponente/>
+    <p>O dado de app é {{ testando }}</p>
+  </div>
+</template>
+
+
+<style scoped>
+</style>
+
+
+
---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+        name: 'PrimeiroComponente',
+        data () {
+            return{
+                nome: "Andrieli", 
+                estudo: "programação"
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <h1>Olá Mundo!</h1>
+        <p>Meu nome é {{ nome }} e estudo programação</p>
+    </div>
+</template>
+
+
